b4211f8fb70e2c586235224160a7148233f9427b
Jifan Zhang <jifan@cs.washington.edu>
2023-03-12 23:16:39 -0500

    Add python environment setup.
    Also fixed class name for clip.

--- ALBench/model/model_impl/clip.py
+++ ALBench/model/model_impl/clip.py
@@ -7,11 +7,11 @@ import clip
 from ALBench.skeleton.model_skeleton import register_model
 
 
-class CLIP_VisionOnly(nn.Module):
+class CLIPVisionOnly(nn.Module):
     def __init__(self, num_output, ret_emb, pretrain, model_name):
 
-        super(CLIP_VisionOnly, self).__init__()
-        assert pretrain, "CLIP_VisionOnly only support pretrain model"
+        super(CLIPVisionOnly, self).__init__()
+        assert pretrain, "CLIPVisionOnly only support pretrain model"
 
         model, preprocess = clip.load(model_name)
         self.image_encoder_model = model
@@ -49,15 +49,15 @@ class CLIP_VisionOnly(nn.Module):
 
 @register_model("clip_vitb16")
 def init_clip_viTB16(model_config, input_dim=None):
-    return CLIP_VisionOnly(model_config["num_output"],
-                           ret_emb=model_config["ret_emb"] if "ret_emb" in model_config else False,
-                           pretrain=model_config["pretrain"] if "pretrain" in model_config else True,
-                           model_name="ViT-B/16")
+    return CLIPVisionOnly(model_config["num_output"],
+                          ret_emb=model_config["ret_emb"] if "ret_emb" in model_config else False,
+                          pretrain=model_config["pretrain"] if "pretrain" in model_config else True,
+                          model_name="ViT-B/16")
 
 
 @register_model("clip_vitb32")
 def init_clip_viTB32(model_config, input_dim=None):
-    return CLIP_VisionOnly(model_config["num_output"],
-                           ret_emb=model_config["ret_emb"] if "ret_emb" in model_config else False,
-                           pretrain=model_config["pretrain"] if "pretrain" in model_config else True,
-                           model_name="ViT-B/32")
+    return CLIPVisionOnly(model_config["num_output"],
+                          ret_emb=model_config["ret_emb"] if "ret_emb" in model_config else False,
+                          pretrain=model_config["pretrain"] if "pretrain" in model_config else True,
+                          model_name="ViT-B/32")
