08ae3ed08a840723e969169f7fe7943123e40d43
yifangc <yifangc@cs.washington.edu>
2023-05-25 16:06:40 -0700

    Add wilds hyperparm tuning.

--- ALBench/metric/metric_impl/multi_class_metric.py
+++ ALBench/metric/metric_impl/multi_class_metric.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from ALBench.skeleton.metric_skeleton import Metric
-
+from sklearn.metrics import f1_score
 
 class MultiClassMetric(Metric):
     metric_name = "multi_class"
@@ -16,6 +16,7 @@ class MultiClassMetric(Metric):
         else:
             preds_label = np.argmax(preds, axis=-1)
             targets_label = np.argmax(targets, axis=-1)
+
         correct = (preds_label == targets_label).astype(float)
         count = np.sum(targets, axis=0)
         for i in range(n_class):
@@ -23,13 +24,17 @@ class MultiClassMetric(Metric):
             acc = np.sum(target * correct) / max(count[i], 1)
             accs.append(acc)
         accs = np.array(accs)
-        return np.sum(accs * count) / np.sum(count), accs
+
+        macro_f1 = f1_score(targets_label, preds_label, average='macro')
+
+        return np.sum(accs * count) / np.sum(count), accs, macro_f1
+    
 
     def compute(self, epoch, preds, labels, losses, val_preds, val_labels, val_losses, test_preds, test_labels,
                 test_losses, num_labeled=None, labeled=None):
-        train_acc, train_accs = self.__accuracy(preds, labels)
-        val_acc, val_accs = self.__accuracy(val_preds, val_labels)
-        test_acc, test_accs = self.__accuracy(test_preds, test_labels)
+        train_acc, train_accs, train_macro_f1 = self.__accuracy(preds, labels)
+        val_acc, val_accs, val_macro_f1 = self.__accuracy(val_preds, val_labels)
+        test_acc, test_accs, test_macro_f1 = self.__accuracy(test_preds, test_labels)
         train_loss = np.mean(losses)
         val_loss = np.mean(val_losses)
         test_loss = np.mean(test_losses)
@@ -43,12 +48,15 @@ class MultiClassMetric(Metric):
                      "Pool Accuracy": train_acc,
                      "Balanced Training Accuracy": np.mean(train_accs),
                      "Pool Loss": train_loss,
+                     "Pool Macro F1": train_macro_f1,
                      "Validation Accuracy": val_acc,
                      "Balanced Validation Accuracy": np.mean(val_accs),
                      "Validation Loss": val_loss,
+                     "Validation Macro F1": val_macro_f1,
                      "Test Accuracy": test_acc,
                      "Balanced Test Accuracy": np.mean(test_accs),
                      "Test Loss": test_loss,
+                     "Test Macro F1": test_macro_f1,
                      "Total Number of Positive": np.sum(num_pos),
                      "Min Class Number of Positive": np.min(num_pos),
                      "Max Class Number of Positive": np.max(num_pos),
