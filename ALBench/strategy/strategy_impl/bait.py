ca199fa3717c42075bcfc81885ba6b3d098d1a49
somussma <somussmann@gmail.com>
2023-06-06 17:06:57 -0500

    BAIT debugging

--- ALBench/strategy/strategy_impl/bait.py
+++ ALBench/strategy/strategy_impl/bait.py
@@ -9,6 +9,15 @@ import torch.nn.functional as F
 from functorch import vmap
 from ALBench.skeleton.active_learning_skeleton import Strategy, ALInput
 
+def mysqrtm(A):
+    lamda, V = np.linalg.eigh(A)
+
+    assert(min(lamda) > -10**-6)
+
+    lamda = np.maximum(0,lamda)
+
+    return V @ np.diag(np.sqrt(lamda)) @ V.T
+
 
 def compute_single_hessian(x, p, ret_embed=False):
     #x, p = np.expand_dims(x, 1), np.expand_dims(p, 1)  # x_single.shape = [embed_dim,1], p_single.shape = [n_classes,1]
@@ -26,18 +35,13 @@ def compute_single_hessian(x, p, ret_embed=False):
             return np.outer(x, x)
     else:
         if ret_embed:
-            lamda, V = np.linalg.eigh(prob_matrix)
-            
-            #print(lamda)
-            assert(min(lamda) > -10**-6)
-            lamda = np.maximum(0,lamda)
-
-            sqrt_prob_matrix = V @ np.diag(np.sqrt(lamda)) @ V.T 
+            sqrt_prob_matrix = mysqrtm(prob_matrix) 
 
             #sqrt_prob_matrix = scipy.linalg.sqrtm(prob_matrix)
             return np.kron(sqrt_prob_matrix, np.expand_dims(x,1))
         else:
             return np.kron(prob_matrix, np.outer(x, x))
+
 
 def symmetrize(A): # for numerics
     return (A + A.T) / 2.0
@@ -46,16 +50,15 @@ def woodbury(A_inv, U, sign):
     big_dim = A_inv.shape[0]
     small_dim = min(U.shape)
 
-    if U.shape != (big_dim, small_dim):
-        U = U.T
+    assert(U.shape == (big_dim, small_dim))
 
     outside = A_inv @ U
-    inside = symmetrize( np.eye(small_dim) + sign * (U.T @ A_inv) @ U )
+    inside = sign * np.eye(small_dim) + (U.T @ A_inv) @ U
 
-    if min(np.linalg.eigvalsh(inside)) < 10**-6:
-        return None
-    else:
-        return symmetrize( A_inv - sign * outside @ np.linalg.inv(inside) @ outside.T )
+    #if min(np.linalg.eigvalsh(inside)) < 10**-6:
+    #    return None
+    #else:
+    return A_inv - outside @ np.linalg.inv(inside) @ outside.T
 
 
 class BAIT(Strategy):
@@ -83,6 +86,9 @@ class BAIT(Strategy):
         # PCA
         embs = embs - np.mean(embs, axis=0)
 
+        evals = np.linalg.eigvalsh(embs.T @ embs)
+        print(evals)
+        
         if self.pca_dim is not None:
             cov = embs.T @ embs / embs.shape[0]
             v, w = np.linalg.eigh(cov)
@@ -90,73 +96,76 @@ class BAIT(Strategy):
 
             embs = embs @ w[:, idx]
 
-        
+        evals = np.linalg.eigvalsh(embs.T @ embs)
+        print(evals)
+      
         labeled_set = set(list(self.dataset.labeled_idxs()))
         all_set = set(list(range(len(self.dataset))))
         unlabeled = list(all_set - labeled_set)
 
         prev_labeled = list(self.dataset.labeled_idxs())
 
-        self.fisher_total = self.compute_fisher(range(len(self.dataset)), preds, embs)
-        self.fisher_total_sqrt = scipy.linalg.sqrtm(self.fisher_total)
-        self.fisher_total_invsqrt = np.linalg.inv(self.fisher_total_sqrt)
+        fisher_total = self.compute_fisher(range(len(self.dataset)), preds, embs)
+
+
+        evals = np.linalg.eigvalsh(fisher_total)
+        print(evals)
+        print(np.sum(evals > 10**-6))
+
+        xxx
+
+        fisher_total_sqrt = mysqrtm(fisher_total)
+        fisher_total_invsqrt = np.linalg.inv(fisher_total_sqrt)
 
         proposed_batch = list(np.random.choice(unlabeled, size=(budget,), replace=False))
 
-        self.inv_fisher_proposed_adj = self.fisher_total_sqrt @ np.linalg.inv(
-            self.compute_fisher(prev_labeled + proposed_batch, preds, embs)) @ self.fisher_total_sqrt
-        self.cur_val = np.trace(self.inv_fisher_proposed_adj)
+        proposed_fisher_ratio = np.linalg.inv(10**-3 * np.eye(embs.shape[1] * preds.shape[1]) + \
+                fisher_total_invsqrt @ self.compute_fisher(prev_labeled + proposed_batch, preds, embs) @ fisher_total_invsqrt)
+        self.cur_val = np.trace(proposed_fisher_ratio)
 
         for _ in range(self.num_complete_swaps):
             for _ in tqdm(range(budget), desc="Swapping"):
                 swap_remove = proposed_batch[0]
 
-                swap_adds = np.random.choice(list(set(unlabeled) - set(proposed_batch)), size=(self.num_proposed_adds,),
-                                             replace=False)
+                swap_adds = list(np.random.choice(list(set(unlabeled) - set(proposed_batch)), size=(self.num_proposed_adds,),
+                                             replace=False))
 
-                remove_U = self.fisher_total_invsqrt @ compute_single_hessian(embs[swap_remove], preds[swap_remove],
+                remove_U = fisher_total_invsqrt @ compute_single_hessian(embs[swap_remove], preds[swap_remove],
                                                                               ret_embed=True)
-                inter_inv_fisher_adj = woodbury(self.inv_fisher_proposed_adj, remove_U, sign=-1)
-                if inter_inv_fisher_adj is not None: # not singular after removal
-                    best_swap_add = swap_remove
-                    best_swap_add_val = self.cur_val
-                    best_swap_matrix = self.inv_fisher_proposed_adj
-                    for swap_add in swap_adds:
-                        add_U = self.fisher_total_invsqrt @ compute_single_hessian(embs[swap_add], preds[swap_add],
+                fisher_ratio_without = woodbury(proposed_fisher_ratio, remove_U, sign=-1)
+                    
+                best_swap_add = swap_remove
+                best_swap_add_val = self.cur_val
+                best_swap_fisher_ratio = proposed_fisher_ratio
+                for swap_add in swap_adds:
+                    add_U = fisher_total_invsqrt @ compute_single_hessian(embs[swap_add], preds[swap_add],
                                                                                ret_embed=True)
 
-                        swap_inv_fisher_adj = woodbury(inter_inv_fisher_adj, add_U, sign=1)
+                    fisher_ratio_swap = woodbury(fisher_ratio_without, add_U, sign=1)
                         
-                        swap_val = np.trace(swap_inv_fisher_adj)
+                    swap_val = np.trace(fisher_ratio_swap)
 
-                        if swap_val < best_swap_add_val:
-                            best_swap_add = swap_add
-                            best_swap_add_val = swap_val
-                            best_swap_matrix = swap_inv_fisher_adj
+                    if False: #swap_val < best_swap_add_val:
+                        best_swap_add = swap_add
+                        best_swap_add_val = swap_val
+                        best_swap_fisher_ratio = fisher_ratio_swap
 
-                    proposed_batch.append(best_swap_add)
-                    del proposed_batch[0]
+                proposed_batch.append(best_swap_add)
+                del proposed_batch[0]
 
-                    self.cur_val = best_swap_add_val
-                    self.inv_fisher_proposed_adj = best_swap_matrix
+                self.cur_val = best_swap_add_val
+                proposed_fisher_ratio = best_swap_fisher_ratio
                 
-                else: # singular matrix!
-                    proposed_batch.append(swap_remove)
-                    del proposed_batch[0]
-
-            print("Woodbury current value: {}".format(self.cur_val))
-            self.inv_fisher_proposed_adj = self.fisher_total_sqrt @ \
-                                           np.linalg.inv(self.compute_fisher(prev_labeled + proposed_batch, preds, embs)) @ \
-                                           self.fisher_total_sqrt
-            self.cur_val = np.trace(self.inv_fisher_proposed_adj)
-            print("Recomputed current value: {}".format(self.cur_val))
+                print("Woodbury current value: {}".format(self.cur_val))
+                proposed_fisher_ratio = np.linalg.inv(10**-3 * np.eye(embs.shape[1] * preds.shape[1]) + \
+                    fisher_total_invsqrt @ self.compute_fisher(prev_labeled + proposed_batch, preds, embs) @ fisher_total_invsqrt)
+                self.cur_val = np.trace(proposed_fisher_ratio)
+                print("Recomputed current value: {}".format(self.cur_val))
 
         return proposed_batch
 
-    def compute_fisher(self, indices, probs, embs):
-        embs = embs[indices]
-        probs = probs[indices]
+    def compute_fisher(self, indices, preds, embs):
         total_fisher = 0
-        for i in tqdm(range(len(indices)), desc="Computing Fisher Information"):
-            total_fisher = total_fisher + compute_single_hessian(embs[i], probs[i])
+        for i in tqdm(indices, desc="Computing Fisher Information"):
+            total_fisher = total_fisher + compute_single_hessian(embs[i], preds[i])
         return total_fisher
